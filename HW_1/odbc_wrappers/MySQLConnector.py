6a4f6c11cba05a08aec2ddfd74b2d277dc37a31d
stevesette <setteducati.s@husky.neu.edu>
2020-01-09 15:06:27 -0500

    added mysql connector class wrapper

--- HW_1/odbc_wrappers/MySQLConnector.py
+++ HW_1/odbc_wrappers/MySQLConnector.py
@@ -9,15 +9,22 @@ class MySQLConnector:
         host="127.0.0.1",
         database="tweets_4300",
     ):
+        print("test")
         self.connection = mysql.connector.connect(
             user=user,
             password=password,
             host=host,
             database=database,
         )
+        self.cursor = self.connection.cursor()
 
     def __enter__(self):
         return self.connection
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.connection.close()
+
+    def insert_one(self, tweet):
+        insert_query = f"INSERT INTO {self.database}.tweets " \
+                       f"(tweet_id, user_id, tweet_ts, tweet_text)" \
+                       f"VALUES {tweet}"
