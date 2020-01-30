161f3b94733b25a01d4b027cd6df2b7039a55b95
stevesette <setteducati.s@husky.neu.edu>
2020-01-30 13:33:28 -0500

    made driver file for inserting followers to any db

---
+++ twitter/follows_insert.py
@@ -0,0 +1,25 @@
+import csv
+
+from utils import pick_db
+
+
+def read_follows(filename):
+    with open(filename, "r") as infile:
+        reader = csv.reader(infile)
+        return [x for x in reader]
+
+
+def upload_all_follows(db_conn, following):
+    with db_conn() as db:
+        for follow in following:
+            db.insert_following(follow)
+
+
+def main():
+    db_type = pick_db(0)
+    following_data = read_follows('following.csv')
+    upload_all_follows(db_type, following_data)
+
+
+if __name__ == '__main__':
+    main()
